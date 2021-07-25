e007835af48b2bd89e06e269ebb8f9848274a57a
Adiel Cardoso <62920887+AdielCardosoDev@users.noreply.github.com>
2021-07-25 14:16:03 -0300

    Classe Conexão

---
+++ JNoticias/JNoticias/Banco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+
+namespace JNoticias
+{
+    class Banco
+    {
+        private static SQLiteConnection conexao;
+
+        private static SQLiteConnection conexaoBanco()
+        {
+            conexao = new SQLiteConnection("Data Source=C:\\Users\\Adiel Cardoso\\Documents\\GitHub\\JNoticias\\JNoticias\\JNoticias\\Banco\\Banco_noticias.db");
+            conexao.Open();
+            
+            return conexao;
+        }
+    }
+}
